fc6ff83349732f53a579b79fd812f9fdbbf2d4f2
aaron-spear-ux <aaronspearux@gmail.com>
2023-02-14 19:01:20 -0700

    base edit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/rainfocus/src/dist/",
+  base: "/rainfocus/",
   plugins: [react()],
   root: "src",
 });
